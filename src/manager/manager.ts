c5ce8858abaf3a7e2276335147a18ee3febfe380
Rohit Saluja <r4saluja@gmail.com>
2021-02-07 02:12:46 +0530

    Initializing the all folder provider

--- src/manager/manager.ts
+++ src/manager/manager.ts
@@ -1,14 +1,25 @@
 import * as vscode from 'vscode';
 import { Logger, getLogger } from '../logger' 
+import { AllFoldersProvider } from './allFoldersProvider';
 
 /**
  * Class for Folder Manager
  */
 export class FolderManager {
-    private logger: Logger;
+    private logger: Logger = getLogger();
+    private afp:AllFoldersProvider | undefined = undefined;
 
     constructor (public wsFolders?:vscode.WorkspaceFolder[]) {
-        this.logger = getLogger();
         this.logger.info('Initializing folder manager');
+        if (wsFolders) {
+            this.initAFP(wsFolders);
+        }
+    }
+
+    private initAFP(wsFolders:vscode.WorkspaceFolder[]) {
+        if (!this.afp) {
+            this.afp = new AllFoldersProvider(wsFolders);
+            vscode.window.registerTreeDataProvider('all-folders', this.afp);
+        }
     }
 }
