b9ea5e5ff369ae48087ad72c2917182c2989cac8
Rohit Saluja <r4saluja@gmail.com>
2021-02-06 16:56:29 +0530

    Adding basic log on activate

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,11 @@
-/** Import necessary modules */
 import * as vscode from 'vscode';
+import { Logger, getLogger } from './logger';
 
 /** this method is called when your extension is activated */
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "workspaces" is now active!');
+    /** Initialize logger */
+    const log:Logger = getLogger()
+    log.info("Workspaces is activated");
 }
 
 /** this method is called when your extension is deactivated */
