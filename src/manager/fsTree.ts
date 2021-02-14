3246eaeadf40d19940221762630f9196e1b82fbc
Rohit Saluja <r4saluja@gmail.com>
2021-02-14 21:39:07 +0530

    Including hidden files

--- src/manager/fsTree.ts
+++ src/manager/fsTree.ts
@@ -39,7 +39,7 @@ export class FsProvider implements vscode.TreeDataProvider<FsTreeItem> {
             if (lstatSync(_path).isDirectory()) {
                 items = readdirSync(_path)
                     .filter(
-                        e => !e.startsWith('.') && !this.filter(join(_path, e))
+                        e => !this.filter(join(_path, e))
                     ).map(
                         e => new FsTreeItem(vscode.Uri.parse(join(_path, e)))
                     );
